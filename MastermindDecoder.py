0b28285b3427794f739fec3e72e5d67e2ac136de
Matthew Weser <matthew.weser@gmail.com>
2022-10-27 06:22:04 -0700

    Start on MasterMind decoder

---
+++ MastermindDecoder.py
@@ -0,0 +1,17 @@
+# Types of values: hits, nicks, and misses
+
+def constraint_4(seq_arr):
+    pass
+
+def sum_values(seq_arr):
+    pass
+
+def seq_to_arr(seq):
+    arr = []
+    for char in str(seq):
+        arr.append(int(char))
+    print(arr)
+
+
+sequence = 12345
+seq_to_arr(sequence)
