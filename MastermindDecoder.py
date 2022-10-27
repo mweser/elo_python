517687ba115021d16f7561a53d63fb0e8577ac23
Matthew Weser <matthew.weser@gmail.com>
2022-10-27 07:12:39 -0700

    Get all solutions

--- MastermindDecoder.py
+++ MastermindDecoder.py
@@ -1,7 +1,14 @@
 # Types of values: hits, nicks, and misses
 
+verbose = False
+line_break = "\n======================\n"
+solutions_arr = []
+
+solution = 57628
+
+
 def constraint_4(sol):
-    print("Constraint 4\n")
+    if verbose: print("Constraint 4\n")
     seq_arr = seq_to_arr(sol)
     return sum_values(seq_arr) == seq_arr[3] * 10 + seq_arr[4]
 
@@ -13,14 +20,14 @@ def sum_values(seq_arr):
     return sum
 
 
-def constraint_3(seq, sol, hits=2, nicks=0):
-    print("Constraint 3\n")
-    return assert_constraint(seq, sol, hits, nicks)
+def constraint_3(sol, hits=2, nicks=0):
+    if verbose: print("Constraint 3\n")
+    return assert_constraint(57319, sol, hits, nicks)
 
 
-def constraint_2(seq, sol, hits=1, nicks=1):
-    print("Constraint 2\n")
-    return assert_constraint(seq, sol, hits, nicks)
+def constraint_2(sol, hits=1, nicks=1):
+    if verbose: print("Constraint 2\n")
+    return assert_constraint(95643, sol, hits, nicks)
 
 
 def assert_constraint(seq, sol, hits, nicks):
@@ -32,9 +39,9 @@ def assert_constraint(seq, sol, hits, nicks):
     return hits == hit_count and nicks == nick_count
 
 
-def constraint_1(seq, sol, hits=0, nicks=1):
-    print("Constraint 1\n")
-    return assert_constraint(seq, sol, hits, nicks)
+def constraint_1(sol, hits=0, nicks=1):
+    if verbose: print("Constraint 1\n")
+    return assert_constraint(79314, sol, hits, nicks)
 
 
 def count_hits(arr1, arr2):
@@ -44,12 +51,13 @@ def count_hits(arr1, arr2):
 
     for i in range(0, size):
         if arr1[i] == arr2[i]:
-            print(f'  Found hit:\n  seq[{i}] -> {arr1[i]}\n\tseq: {arr1}\n\tsol: {arr2} \n')
+            if verbose: print(
+                f'  Found hit:\n  seq[{i}] -> {arr1[i]}\n\tseq: {arr1}\n\tsol: {arr2} \n')
             hit_count += 1
         else:
             for j in range(0, size):
                 if arr1[i] == arr2[j] and i != j:
-                    print(
+                    if verbose: print(
                         f'  Found nick:\n  seq[{i}] & sol[{j}] -> {arr1[i]}\n\tseq: {arr1}\n\tsol: {arr2} \n')
 
                     nick_count += 1
@@ -64,18 +72,38 @@ def seq_to_arr(seq):
     return arr
 
 
-solution = 57628
-line_break = "\n======================\n"
-# sequence = 12345
-# seq_arr = seq_to_arr(sequence)
+def sample_check():
+    if verbose: print(f'Result: {constraint_1(solution)}{line_break}')
+    if verbose: print(f'Result: {constraint_2(solution)}{line_break}')
+    if verbose: print(f'Result: {constraint_3(solution)}{line_break}')
 
-sequence = 79314
-print(f'Result: {constraint_1(sequence, solution)}{line_break}')
+    if verbose: print(
+        f'Result: {constraint_4(solution)} \t\t\t(A + B + C + D + E = D * 10 + E){line_break}')
 
-sequence = 95643
-print(f'Result: {constraint_2(sequence, solution)}{line_break}')
 
-sequence = 57319
-print(f'Result: {constraint_3(sequence, solution)}{line_break}')
+def check_constraints(attempt):
+    if not constraint_1(attempt):
+        if verbose: print(f'{attempt} Failed at Constraint 1\n')
+        return
 
-print(f'Result: {constraint_4(solution)} \t\t\t(A + B + C + D + E = D * 10 + E){line_break}')
+    if not constraint_2(attempt):
+        if verbose:   print(f'{attempt} Failed at Constraint 2\n')
+        return
+
+    if not constraint_3(attempt):
+        if verbose:  print(f'{attempt} Failed at Constraint 3\n')
+        return
+
+    if not constraint_4(attempt):
+        if verbose: print(f'{attempt} Failed at Constraint 4\n')
+        return
+
+    if verbose: print(f'{attempt} Passed all constraints! ')
+    solutions_arr.append(attempt)
+    return True
+
+
+for solution in range(10000, 100000):
+    check_constraints(solution)
+
+print(f'Solutions:\n{solutions_arr}')
